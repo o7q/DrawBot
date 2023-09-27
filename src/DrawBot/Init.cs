df394cbe63b8ecf550133c21333c069c767ced6a
o7q <jev.o7q@gmail.com>
2023-09-27 13:59:20 -0500

    Create Init.cs

---
+++ src/DrawBot/Init.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DrawBot
+{
+    internal static class Init
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainMenu());
+        }
+    }
+}
